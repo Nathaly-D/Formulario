bde17f7052acfb2756e698078feba380432b46e2
Nathaly-D <113744119+Nathaly-D@users.noreply.github.com>
2023-02-17 23:27:32 -0500

    Segunda

--- dist/main.js
+++ dist/main.js
@@ -1,34 +1,74 @@
 'use srtric';
 import{validateString, vAvg, checkAvg} from './validate.js';
-import{paintCard,addStudent,modalAlert, modalAlert2} from './paint.js';
+import{paintCard,addStudent,addProfesor, modalAlert} from './paint.js';
 
 const btnAgregar = document.getElementById('btnAgregar');
 const btnMostrar = document.getElementById('btnMostrar');
+const opt = document.getElementById('opcion').value;
 
-//Estudiante
+
+opt.onchange = function (){
+    let op = document.getElementById('opcion').value; 
+    if (op === 'profesor'){
+        modalAlert("Los datos han cambiado a profesor")
+        document.getElementById('text1').innerText="Nombre completo:";
+        document.getElementById('text2').innerText="Profesión:";
+        document.getElementById('text3').innerText="Edad:";
+    } else if (op === 'estudiante'){
+        modalAlert("Los datos han cambiado a estudiante")
+        document.getElementById('text1').innerText="Nombres:";
+        document.getElementById('text2').innerText="Apellidos:";
+        document.getElementById('text3').innerText="Promedio:";
+    }
+}
+
+///////////////////////////////////////
 btnAgregar.onclick = function(){
     const name = document.getElementById('nombre').value;
     const lastName =document.getElementById('apellido').value;
     const avg = parseFloat (document.getElementById('promedio').value);
+    const nameC = document.getElementById('nombre').value;
+    const pro =document.getElementById('apellido').value;
+    const edad =parseFloat(document.getElementById('promedio').value);
     const op = document.getElementById('opcion').value;
     
     //console.log(`${name} ${lastName} ${avg} ${op}`);
-    if(validateString(name) && validateString(lastName) && op != 0){
-        if((!isNaN(avg)) && (vAvg(avg))){
-            addStudent(name,lastName,avg)
-        }else{
-            document.querySelector('#promedio').value="";
-            modalAlert("Promedio Invalido")
+    if(op === 'estudiante'){
+        if(validateString(name) && validateString(lastName) && op != 0){
+            if((!isNaN(avg)) && (vAvg(avg))){
+                addStudent(name,lastName,avg)
+                modalAlert("Estudiante agregado")
+                document.getElementById('nombre').value="";
+                document.getElementById('apellido').value="";
+                document.querySelector('#promedio').value="";
+            } else {
+                document.querySelector('#promedio').value="";
+                modalAlert("Promedio Invalido");
+            }
+        } else {
+            modalAlert("Datos Invalidos, revisar los datos");
         }
-    }else{
-        modalAlert("Datos Invalidos, revisar los datos");
-    }
-    formulario.reset(); 
+    } else if (op === 'profesor') {
+        if (validateString(nameC) && validateString(pro) && op != 0){
+            if (!isNaN(edad)){ 
+                addProfesor(name,lastName,avg)
+                modalAlert("Profesor agregado")
+                document.getElementById('nombre').value="";
+                document.getElementById('apellido').value="";
+                document.querySelector('#promedio').value="";
+            }
+        }else{
+            modalAlert("Datos Invalidos, revisar los Campos");
+        }
+    }   
 }
 
 
-btnMostrar.addEventListener("click", function(){
-    paintCard("ESTUDIANTE");
-
+btnMostrar.addEventListener('click', function(){
+    const op = document.getElementById('opcion').value;
+    if (op === 'estudiante'){
+        paintCard("ESTUDIANTE");
+    } else if (op === 'profesor') {
+        paintCard("PROFESOR");
+    }  
 });
- 
--- dist/paint.js
+++ dist/paint.js
@@ -1,8 +1,7 @@
-'use strict'
 import { checkAvg } from './validate.js';
 const cardE = document.getElementById('cardsEstudiantes');
-const students = [];
 const cardP =document.getElementById('cardseProfesores') ;
+const students = [];
 const profesores = [];
 
 const paintCard = (typ) =>{
@@ -20,20 +19,17 @@ const paintCard = (typ) =>{
             cloneTemp.querySelector('.text-aprobado').innerHTML = `${checkAvg(i.prom)}`;
             fragment.appendChild(cloneTemp);
         }
-    }else{
+    }else if (typ === 'PROFESOR') ;{
         for(let i of profesores){
             const cloneTemp = templateProfesor.cloneNode(true);
             cloneTemp.querySelector('.title-card').innerHTML = "Datos del <i> Profesor </i>";
-            cloneTemp.querySelector('.data-card').innerHTML = `NOMBRE: ${i.nom.toUpperCase()} APELLIDOS: ${i.ape.toUpperCase()}`;
-            cloneTemp.querySelector('.text-profesion').innerHTML = `PROFESIÓN ES: ${i.prof}`;
+            cloneTemp.querySelector('.data-card').innerHTML = `NOMBRE COMPLETO: ${i.nameC.toUpperCase()} LA PROFESIÓN ES: ${i.pro.toUpperCase()}`;
+            cloneTemp.querySelector('.text-promedio').innerHTML = `LA EDAD ES: ${i.edad.toUpperCase()}`;
             fragment.appendChild(cloneTemp);
         }
-
     }
-    cardE.appendChild(fragment);{
-        modalAlert('Mostrando usuario');
-    }
-   
+    cardE.appendChild(fragment);
+    cardP.appendChild(fragment);
 };
 
 
@@ -45,10 +41,15 @@ const addStudent = (name, lastName, avg) =>{
         prom : avg
     }
     students.push(student);
-    modalAlert('Se agregó  usuario');
-
-};
-
+}
+const addProfesor = (name, lastName, avg) =>{
+    let profesor ={
+        nameC : name, 
+        pro : lastName,
+        edad : avg
+    }
+    profesores.push(profesor);
+}
 
 const modalAlert = (cad) => {
     const alerta = document.createElement('div');
@@ -64,25 +65,9 @@ const modalAlert = (cad) => {
     alerta.appendChild(texto);
     document.body.appendChild(alerta);
     img.onclick = function(){
-        document.getElementById("alerta"). remove ();
+        document.getElementById("alerta"). remove();
     }
 }
 
-const modalAlert2 = (cad) => {
-    const alerta = document.createElement('div');
-    const texto = document.createElement('p');
-    const img = document.createElement('img');
-    img.src = "./img/MicrosoftTeams-image (2).png";
-    img.className = "close";
-    texto.setAttribute("class", "textAlerta");
-    alerta.setAttribute("id","alerta2");
-    alerta.setAttribute("class", "alerta2");
-    texto.innerHTML = `<strong> ${cad} </strong>`;
-    alerta.appendChild(img);
-    alerta.appendChild(texto);
-    document.body.appendChild(alerta);
-    img.onclick = function(){
-        document.getElementById("alerta2"). remove ();
-    }
-}
-export { paintCard,addStudent,modalAlert, modalAlert2  }
+
+export { paintCard,addProfesor,addStudent,modalAlert}
